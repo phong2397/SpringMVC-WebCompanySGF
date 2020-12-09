46c1c674b8f22a64cdebe54be9b1c01a42147283
matkinhig <matkinhig@gmail.com>
2020-12-09 17:33:56 +0700

    + init controller for Phong

--- src/main/java/com/sgfintech/controller/ApprovalController.java
+++ src/main/java/com/sgfintech/controller/ApprovalController.java
@@ -1,10 +1,17 @@
 package com.sgfintech.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author lucnguyen.hcmut@gmail.com
  */
 @Controller
 public class ApprovalController {
+    @RequestMapping(value = { "/xetduyet" }, method = RequestMethod.GET)
+    public String welcomePage(Model model) {
+        return "xetduyet";
+    }
 }
--- src/main/java/com/sgfintech/controller/SignController.java
+++ src/main/java/com/sgfintech/controller/SignController.java
@@ -1,10 +1,17 @@
 package com.sgfintech.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author lucnguyen.hcmut@gmail.com
  */
 @Controller
 public class SignController {
+    @RequestMapping(value = { "/kyduyet" }, method = RequestMethod.GET)
+    public String welcomePage(Model model) {
+        return "kyduyet";
+    }
 }
