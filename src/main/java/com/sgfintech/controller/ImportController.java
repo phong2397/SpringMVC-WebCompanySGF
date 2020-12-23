595d853b61b642f1e20253a664e7f21e0100ade6
matkinhig <matkinhig@gmail.com>
2020-12-23 16:57:16 +0700

    - upload document include excel

--- src/main/java/com/sgfintech/controller/ImportController.java
+++ src/main/java/com/sgfintech/controller/ImportController.java
@@ -7,23 +7,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.List;
-
-import javax.servlet.ServletContext;
 import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author lucnguyen.hcmut@gmail.com
  */
 @Controller
 public class ImportController implements ServletContextAware {
+
+    String fileLocation = "file-upload/archived_system/";
+
     @RequestMapping(value = {"/import"}, method = RequestMethod.GET)
     public String welcomePage(Model model) {
         return "import";
@@ -33,30 +37,33 @@ public class ImportController implements ServletContextAware {
     private ServletContext servletContext;
 
     @RequestMapping(value = "/import", method = RequestMethod.POST)
-    public String process(@RequestParam("file") MultipartFile file) throws Exception {
-        String fileName = uploadExcelFile(file);
-        System.out.println("File Name: " + fileName);
-        String excelPath = servletContext.getRealPath("/resources/excels/" + fileName);
-        System.out.println("Excel Path: " + excelPath);
-        ExcelHelper excelHelper = new ExcelHelper(excelPath);
-        List<Customer> products = excelHelper.readData(Customer.class.getName());
-        System.out.println("Product List");
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        for (Customer product : products) {
-            product.toString();
+    public String process(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request) throws Exception {
+        String companyCode = request.getParameter("code");
+        String companyName = request.getParameter("name");
+        try {
+            String path = servletContext.getRealPath(fileLocation);
+//        String fileName = uploadExcelFile(file, path);
+            Date date = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-sss");
+            String fileName = sdf.format(date) + multipartFile.getOriginalFilename();
+            File file = new File(path, fileName);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            multipartFile.transferTo(file);
+            String excelPath = servletContext.getRealPath(fileLocation + fileName);
+            ExcelHelper excelHelper = new ExcelHelper(excelPath);
+            List<Customer> lstCustomer = excelHelper.parseDataFromExcel("Customer", companyCode);
+            System.out.println("Customer List");
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            for (Customer item : lstCustomer) {
+                item.toString();
+            }
+        } catch (Exception ex) {
+            ex.toString();
+            ex.printStackTrace();
         }
         return "product/index";
-    }
-
-    private String uploadExcelFile(MultipartFile multipartFile) {
-        try {
-            byte[] bytes = multipartFile.getBytes();
-            Path path = Paths.get(servletContext.getRealPath("/resources/excels/" + multipartFile.getOriginalFilename()));
-            Files.write(path, bytes);
-            return multipartFile.getOriginalFilename();
-        } catch (Exception e) {
-            return null;
-        }
     }
 
     @Override
